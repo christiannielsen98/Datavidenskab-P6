56d94176cf0be79676c9236055fecce661bd4425
MartinGejlLund <56994736+MartinGejlLund@users.noreply.github.com>
2022-02-01 11:32:24 +0100

    base setup

---
+++ Project/DatabaseLocator.py
@@ -0,0 +1,48 @@
+import configparser
+import os
+
+from project_root import project_path
+
+
+class DatabaseLocator:
+    def __init__(self):
+        """
+        This constructor method creates container attributes for directory paths.
+        config_path is hard coded.
+        database_path is loaded from the Config file.
+        """
+        self.__config_path = os.path.join(project_path, 'Config')
+        self.__onedrive_path = self.__config_load()
+        self.__model_path = os.path.join(project_path, 'Project', 'Models')
+
+    def __config_load(self):
+        """
+        Loads database path.
+        """
+        config = configparser.ConfigParser()
+
+        config.read(os.path.join(self.__config_path, os.listdir(self.__config_path)[0]))
+
+        return config['onedrive']['path']
+
+    def get_onedrive_path(self, sub_folder=None):
+        """
+        Returns the directory path to the database.
+        """
+        path = self.__onedrive_path
+
+        if sub_folder is None:
+            return path
+
+        else:
+            if isinstance(sub_folder, (list, tuple)):
+                return os.path.join(path, *sub_folder)
+            elif "\\" in sub_folder:
+                return os.path.join(path, *sub_folder.split("\\"))
+            else:
+                return os.path.join(path, *sub_folder.split("/"))
+
+
+if __name__ == '__main__':
+    Locator = DatabaseLocator()
+    print(Locator.__dict__)
---
+++ project_root.py
@@ -0,0 +1,4 @@
+project_path = __file__.split('project_root.py')[0]
+
+if __name__ == "__main__":
+    print(project_path)
