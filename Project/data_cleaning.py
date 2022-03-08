a64634464a8a630eabab1f13970e7ace863b352a
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2022-03-08 12:05:04 +0100

    Update data_cleaning.py

--- Project/data_cleaning.py
+++ Project/data_cleaning.py
@@ -8,17 +8,17 @@ for hourly in [True, False]:
         time_base = "hour" if hourly else "minute"
         house, meta = Db.load_data(hourly=hourly, meta=True, year=year)
 
-        # meta.set_index("Unnamed: 0", inplace=True)
-        # meta = meta.loc[~((~pd.isnull(house)).sum(0) <= (house.shape[0] / 2))]
-        #
-        # status_condition = (lambda self: self["Units"] == "Binary Status")
-        # status_columns = meta.loc[status_condition].index.tolist()
-        #
-        # house[(house[status_columns] != 0) & (house[status_columns] != 1)] = np.NaN
-        # house = house.ffill()
-        #
-        # meta = meta.loc[(house != house.shift(1)).sum(0) > 1]
-        # house = house[meta.index]
+        meta.set_index("Unnamed: 0", inplace=True)
+        meta = meta.loc[~((~pd.isnull(house)).sum(0) <= (house.shape[0] / 2))]
+
+        status_condition = (lambda self: self["Units"] == "Binary Status")
+        status_columns = meta.loc[status_condition].index.tolist()
+
+        house[(house[status_columns] != 0) & (house[status_columns] != 1)] = np.NaN
+        house = house.ffill()
+
+        meta = meta.loc[(house != house.shift(1)).sum(0) > 1]
+        house = house[["Timestamp"] + meta.index.tolist()]
 
         house["Timestamp"] = pd.to_datetime(house["Timestamp"], format="%Y-%m-%d %H:%M:%S%z")
 
