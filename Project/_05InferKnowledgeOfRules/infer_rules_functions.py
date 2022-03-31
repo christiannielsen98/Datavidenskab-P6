ec550de3b4f37f174cf831e8825ff4f88882d48b
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2022-03-31 10:41:00 +0200

    Update infer_rules_functions.py

--- Project/_05InferKnowledgeOfRules/infer_rules_functions.py
+++ Project/_05InferKnowledgeOfRules/infer_rules_functions.py
@@ -8,24 +8,17 @@ import numpy as np
 import json
 from Project.Database import Db
 
-level1_json = json.load(
-    open(Db.get_project_path("Project/_04TPMAlgorithm/TPM/output/NZERTF_year1_minsup0.14_minconf_0.5/level1.json")))
 
-level2_json = json.load(
-    open(Db.get_project_path("Project/_04TPMAlgorithm/TPM/output/NZERTF_year1_minsup0.14_minconf_0.5/level2.json")))
-
-level3_json = json.load(
-    open(Db.get_project_path("Project/_04TPMAlgorithm/TPM/output/NZERTF_year1_minsup0.14_minconf_0.5/level3.json")))
-
-def json_to_dataframe(filename):
-    json_file = json.load(open(filename))
+def json_to_dataframe(year, level):
+    json_file = json.load(
+        open(f"Project/_04TPMAlgorithm/TPM/output/NZERTF_year{year}_minsup0.14_minconf_0.5/level{level}.json"))
     if "," in json_file[0]["name_node"]:
-        level_df = pd.DataFrame(columns=["pattern","supp","conf", "time"])
+        level_df = pd.DataFrame(columns=["pattern", "supp", "conf", "time"])
         for i in json_file:
             for j in i["patterns"]:
                 level_df.loc[level_df.shape[0]] = j
     else:
-        level_df = pd.DataFrame(columns=["name_node","supp","conf"])
+        level_df = pd.DataFrame(columns=["name_node", "supp", "conf"])
         for i in json_file:
             level_df.loc[level_df.shape[0]] = i
     return level_df
