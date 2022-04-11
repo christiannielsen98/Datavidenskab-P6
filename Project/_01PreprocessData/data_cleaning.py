d6df7de6ca6b768acf93a24840ad13b598d81352
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2022-04-11 15:25:57 +0200

    cleaning in files

--- Project/_01PreprocessData/data_cleaning.py
+++ Project/_01PreprocessData/data_cleaning.py
@@ -37,14 +37,16 @@ def data_cleaner(hourly, year):
     status_condition = (lambda self: self['Units'] == 'Binary Status')
     status_columns = meta.loc[status_condition].index.tolist()
 
-    house[(house[status_columns] != 0) & (house[status_columns] != 1)] = np.NaN
-    house = house.ffill()
+    # house[(house[status_columns] != 0) & (house[status_columns] != 1)] = np.NaN
+    # house = house.ffill()
+    house[status_columns] = np.where(house[status_columns] > 0, 1, 0)
 
     # meta = meta.loc[(house != house.shift(1)).sum(0) > 1]
     house = house[['Timestamp'] + meta.index.tolist()]
 
     house['Timestamp'] = house['Timestamp'].str.split('-0[45]:00', expand=True)[0]
     house['Timestamp'] = pd.to_datetime(house['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+
 
     house['Load_StatusRefrigerator'] = np.where(house['Elec_PowerRefrigerator'] > 0, 1, 0)
     house['Load_StatusMicrowave'] = np.where(house['Elec_PowerMicrowave'] > 0, 1, 0)
