8092656402c0075fb99d78ab9b11bacf0092295b
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2022-04-12 08:16:19 +0200

    Update data_cleaning.py

--- Project/_01PreprocessData/data_cleaning.py
+++ Project/_01PreprocessData/data_cleaning.py
@@ -28,6 +28,7 @@ room_floor_dict = {
     'Utility': '1stFloor'
 }
 
+
 def data_cleaner(hourly, year):
     house, meta = Db.load_data(hourly=hourly, meta=True, year=year, from_csv=True)
 
@@ -46,7 +47,6 @@ def data_cleaner(hourly, year):
 
     house['Timestamp'] = house['Timestamp'].str.split('-0[45]:00', expand=True)[0]
     house['Timestamp'] = pd.to_datetime(house['Timestamp'], format='%Y-%m-%d %H:%M:%S')
-
 
     house['Load_StatusRefrigerator'] = np.where(house['Elec_PowerRefrigerator'] > 0, 1, 0)
     house['Load_StatusMicrowave'] = np.where(house['Elec_PowerMicrowave'] > 0, 1, 0)
